af943620a91c543ae1c112327e96745dafb14137
baa <34665120+baa-mb@users.noreply.github.com>
2020-10-01 01:14:36 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,6 +19,7 @@ basic.forever(function () {
                 . . # . .
                 . . # . .
                 `)
+            basic.pause(1000)
             basic.showLeds(`
                 # . . . #
                 # . . . #
